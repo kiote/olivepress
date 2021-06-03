baf0b4736db8321bd308f209b1c84d56f55571dd
kiote <krivich.ekaterina@gmail.com>
2021-06-03 11:41:20 +0300

    feat: add coinbase api app (empty)

---
+++ apps/api_coinbase/lib/api_coinbase.ex
@@ -0,0 +1,18 @@
+defmodule ApiCoinbase do
+  @moduledoc """
+  Documentation for `ApiCoinbase`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ApiCoinbase.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/api_coinbase/test/api_coinbase_test.exs
@@ -0,0 +1,8 @@
+defmodule ApiCoinbaseTest do
+  use ExUnit.Case
+  doctest ApiCoinbase
+
+  test "greets the world" do
+    assert ApiCoinbase.hello() == :world
+  end
+end

be5e2628ee531078f1e43722c016364398b40353
kiote <krivich.ekaterina@gmail.com>
2021-06-09 20:01:11 +0300

    feat: add more docs

--- apps/strategies/lib/weekly.ex
+++ apps/strategies/lib/weekly.ex
@@ -1,8 +1,7 @@
 defmodule Startegy.Weekly do
   @moduledoc """
-  Simpliest strategy - buy every week.any()
-
-  If today is _the day_ - Wednesday by default, gives an advice to buy,
+  Simpliest strategy - buy every week.
+  If today is _the day_ - Tuesday by default, gives an advice to buy,
   otherwise - skip.
   """
   @behaviour Strategy
@@ -10,12 +9,17 @@ defmodule Startegy.Weekly do
   alias Startegy.Weekly.ParamsConverter
 
   defp day_to_buy do
-    Application.get_env(:strategies, :weekly, weekday: :tue)
+    Application.get_env(:strategies, :weekly, weekday: :wed)
     |> Enum.into(%{})
     |> ParamsConverter.convert()
     |> Map.fetch!(:weekday)
   end
 
+  @doc """
+  Based on day of a week advice.
+  Consider calling weekly-based strategies once per 24h
+  otherwise you'd buy as many times as it is called
+  """
   @impl true
   def advice(%{weekday: weekday}) when is_atom(weekday) do
     if weekday == day_to_buy() do
@@ -25,6 +29,7 @@ defmodule Startegy.Weekly do
     end
   end
 
+  # Making sure that string parameters also workS
   def advice(params) do
     case ParamsConverter.convert(params) do
       %{weekday: weekday} = converted when is_atom(weekday) ->
